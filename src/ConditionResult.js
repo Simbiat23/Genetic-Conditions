ec8b0f92594660b081dd0a4f63e7be0f33508f41
Simbiat23 <155582634+Simbiat23@users.noreply.github.com>
2025-09-08 16:27:29 +0100

    added the display results

--- src/ConditionResult.js
+++ src/ConditionResult.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 export default function ConditionResult({ results }) {
+  console.log({ results });
   return (
     <div>
-      <h3>Gene: {results.display_name}</h3>
-      <p>Description:{results.description}</p>
-
+      <h1>{results.display_name}</h1>
+      <p>{results.description}</p>
       <p>
-        Description:{results.seq_region_name}:{results.start}-{results.end}
+        {results.seq_region_name}:{results.start}-{results.end}
       </p>
     </div>
   );
